dc85df3695130c7e7891ae93302ce9fb04e6d458
qundax <qunda.lim@outlook.sg>
2020-06-02 09:54:32 +0800

    initial commit

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import ToDoList from './components/todolist/todolist';
+
 class App extends React.Component {
   render() {
     return (
       <div>
         Welcome.
+        <ToDoList />
       </div>
     );
   }
